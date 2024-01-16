10e87b7ae3aa3c879e3d98acada0f2fe57deadea
jgalicia <jgalicia-2022203@kinal.edu.gt>
2024-01-16 15:55:51 -0600

    Second Commit

--- hangman.js
+++ hangman.js
@@ -1,11 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+const words = [
+  "programming",
+  "javascript",
+  "development",
+  "Guatemala",
+  "computer",
+  "technology",
+  "application",
+  "learning",
+  "project",
+  "student",
+  "code",
+  "web",
+  "game",
+  "programmer",
+  "application",
+  "feedback",
+  "career",
+  "challenge",
+  "solution",
+  "algorithm",
+  "function",
+  "bug",
+  "team",
+  "innovation",
+  "framework",
+];
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const buttonsContainer = document.getElementById("buttons-container");
+const usedLettersContainer = document.getElementById("used-letters");
+const attemptsRemainingContainer =
+  document.getElementById("attempts-remaining");
+const wordContainer = document.getElementById("word-container");
+const restartButton = document.getElementById("restart-button");
+const usedLetters = [];
+const maxAttempts = 7;
+const canvas = document.getElementById("hangman-canvas");
+const ctx = canvas.getContext("2d");
+
+let attemptsRemaining = maxAttempts;
+let selectedWord = selectRandomWord(words);
+let gameInProgress = false;
